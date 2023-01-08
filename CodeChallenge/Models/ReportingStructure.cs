c817c3b36c7159f6d28060025203f8be0ad6163c
stynan27 <seamustynan16@gmail.com>
2023-01-08 16:06:16 -0500

    Task 1 Reporting Structure model

---
+++ CodeChallenge/Models/ReportingStructure.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Models
+{
+    // Our type definition for a ReportingStructure
+    public class ReportingStructure
+    {
+        public Employee employee { get; set; }
+        public int numberOfReports { get; set; }
+    }
+}
